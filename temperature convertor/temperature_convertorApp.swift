3e17ea7fa7a5807424b6a0ff2afae32db2c7db2c
Shivani R <shivanir.1901219@srec.ac.in>
2021-09-03 15:42:00 +0530

    Initial Commit

---
+++ temperature convertor/temperature_convertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperature_convertorApp.swift
+//  temperature convertor
+//
+//  Created by user on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct temperature_convertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
